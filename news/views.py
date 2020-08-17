c80f9bf1641bdcbd199fa4a77d438ca9c3aa1dad
carlokruger <oUtME7tNxmfRnLxwgTxvUiUJ>
2020-08-17 14:03:11 +1200

    completed Stage 1

--- news/views.py
+++ news/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views import View
 
 # Create your views here.
+
+
+class IndexView(View):
+    template_name = "news/index.html"
+
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name)
