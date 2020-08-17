0bdb9214ef7e2f5890c9ec38fbc646cf5e320559
carlokruger <oUtME7tNxmfRnLxwgTxvUiUJ>
2020-08-17 14:54:25 +1200

    completed Stage 2

--- hypernews/urls.py
+++ hypernews/urls.py
@@ -14,10 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from news.views import IndexView
+from django.urls import path, re_path
+from news.views import IndexView, NewsView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', IndexView.as_view()),
+    re_path(r'^news/[0-9]+', NewsView.as_view())
 ]
--- news/views.py
+++ news/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from django.views import View
+import json
+from hypernews.settings import NEWS_JSON_PATH
 
 # Create your views here.
+
+
+with open(NEWS_JSON_PATH, "r") as json_file:
+    news = json.load(json_file)
 
 
 class IndexView(View):
@@ -9,3 +15,16 @@ class IndexView(View):
 
     def get(self, request, *args, **kwargs):
         return render(request, self.template_name)
+
+
+class NewsView(View):
+    template_name = "news/news.html"
+
+    def get(self, request, *args, **kwargs):
+        global news
+        title = news[0]["title"]
+        created = news[0]["created"]
+        text = news[0]["text"]
+        link = news[0]["link"]
+        vars = {"title": title, "created": created, "text": text, "link": link }
+        return render(request, self.template_name, vars)
